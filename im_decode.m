4f82c9ed5419d5bfabf31e489b09662fca3a4095
Graham Holland <graham.holland@gmail.com>
2011-11-11 19:03:36 -0800

    added barebones encode and decode functions

---
+++ im_decode.m
@@ -0,0 +1,6 @@
+function [] = im_decode(bitstream_name, dec_name, N_images)
+%IM_DECODE Summary of this function goes here.
+%   [] = IM_DECODE() Detailed explanation goes here.
+
+
+end
---
+++ im_encode.m
@@ -0,0 +1,6 @@
+function [] = im_encode(name, bitstream_name, N_images )
+%IM_ENCODE Summary of this function goes here.
+%   [] = IM_ENCODE(INPUT_ARGS) Detailed explanation goes here.
+
+
+end
