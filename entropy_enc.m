5108ae667117dd61b32f7ee035ab9e6d720ea3da
Graham Holland <graham.holland@gmail.com>
2011-11-11 23:28:20 -0800

    finished implementation of im_encode following lab 5

---
+++ entropy_enc.m
@@ -0,0 +1,73 @@
+function [] = entropy_enc(frame_h, frame_w, frameq, bitstream_name, N_images, quality)
+%ENTROPY_ENC Summary of this function goes here.
+%   [] = ENTROPY_ENC(INPUT_ARGS) Detailed explanation goes here.
+%
+%   Bitstream will contain:
+%       general info:
+%           N_images        uint8
+%           frame_h         uint16
+%           frame_w         uint16
+%           quality         uint8
+%       for each frame:
+%           min_index       uint8
+%           length(counts)  uint8
+%           counts          uint32
+%           Nbits           uint32
+%           imgq_enc        ubit1
+
+% open bitstream for writing
+fid = fopen(bitstream_name, 'w');
+
+% write frame_h, frame_w and quality to the bitstream
+fwrite(fid, N_images, 'uint8');
+fwrite(fid, [frame_h frame_w], 'uint16');
+fwrite(fid, quality, 'uint8');
+
+% encode each frame and add to bitstream
+for k = 1:N_images
+
+    % get imgq from frameq
+    imgq = frameq(:,:,k);
+    
+    % get number of rows and columns in imgq
+    [imgq_r, imgq_c] = size(imgq);
+
+    % convert all elements in imgq to positive integers by adding min element
+    % to all elements
+    min_index = min(min(imgq));
+    if (min_index < 0)
+        imgq = imgq + abs(min_index) + 1; % pos integers don't include 0
+    end
+
+    % convert imgq to row vector by stringing rows together
+    imgq = reshape(imgq, 1, imgq_r*imgq_c);
+
+    % get histogram for imgq
+    counts = hist(imgq, max(imgq));
+
+    % replace 0s with 1s in counts
+    ind = find(counts == 0);
+    if ~(isempty(ind))
+        counts(ind) = 1;
+    end
+
+    % write min index to bitstream
+    fwrite(fid, min_index, 'uint8');
+
+    % write historgam and histogram size to bitstream
+    fwrite(fid, length(counts), 'uint8');
+    fwrite(fid, counts, 'uint32');
+
+    % encode imgq using arithmetic encoding
+    imgq_enc = arithenco(imgq, counts);
+    Nbits = length(imgq_enc);
+
+    % write encoded bitstream
+    fwrite(fid, Nbits, 'uint32');
+    fwrite(fid, imgq_enc, 'ubit1');
+
+end
+
+fclose(fid);
+
+end
--- im_encode.m
+++ im_encode.m
@@ -1,6 +1,90 @@
-function [] = im_encode(name, bitstream_name, N_images )
+function [] = im_encode(name, bitstream_name, N_images, quality)
 %IM_ENCODE Summary of this function goes here.
 %   [] = IM_ENCODE(INPUT_ARGS) Detailed explanation goes here.
 
+% read in the first image to get the resolution for the set
+frame = imread(strcat(name, '0.pgm'));
+[M, N] = size(frame);
+
+% create  zero M-by-N-by-N_images array
+frame = zeros(M, N, N_images);
+
+% read in the rest of the images given name and N_images
+% note: images in sets start numbering at 0
+for k = 1:N_images
+    frame(:,:,k) = imread(strcat(name, num2str(k-1), '.pgm'), 'pgm');
+
+    % convert to double and centre values about 0
+    frame(:,:,k) = double(frame(:,:,k)) - 128;
+end
+
+% perform DCT and quantization to each frame
+% get quantization and zig-zag matrices
+[qt, zag] = init_jpeg(quality);
+
+% initialize frame_q M*N/64-by-64-by-N_images array to hold quantized DCT
+% coefficients for each block within each image
+frameq = zeros(M*N/64, 64, N_images);
+
+% get function handle for 2 dimensional DCT
+dct2fun = @dct2;
+
+for k = 1:N_images
+    % get image from frame
+    img = frame(:,:,k);
+    
+    % perform 8x8 block DCT to image
+    dct_mat = blkproc(img, [8 8], dct2fun);
+
+    % subdivide dct_mat to a 4D array Y such that:
+    % dct_mat = [   Y(:,:,1,1)    Y(:,:,1,2)  ...  Y(:,:1,N/8)
+    %               Y(:,:,2,1)    Y(:,:,2,2)  ...  Y(:,:2,N/8)
+    %                  ...            ...     ...      ...
+    %              Y(:,:,M/8,1)  Y(:,:,M/8,2) ... Y(:,:M/8,N/8) ];
+    Y = reshape(dct_mat, [8, M/8, 8, N/8]);
+    Y = permute(Y, [1 3 2 4]);
+
+    % initialize matrix imgq to hold quantized DCT coefficients. Each 8x8
+    % subblock scanning through img from right to left, up to down gets 
+    % a single 64 column row.
+    imgq = zeros(M*N/64, 64);
+
+    % index for which row (block) we are operating on for imgq
+    blkcnt = 1;
+
+    % fill up imgq rows with the quantized DCT coefficients for the 
+    % appropriate 8x8 block
+    for i = 1:M/8
+        for j = 1:N/8
+
+            % convert DCT coefficients to row vector using zig zag scan
+            imgq_row = imgq(blkcnt,:);
+            imgq_row(zag) = Y(:,:,i,j);
+
+            % quantize the row vector
+            imgq(blkcnt,:) = round(imgq_row./qt);
+
+            blkcnt = blkcnt + 1;
+        end
+    end
+
+    % get number of rows and columns in imgq
+    [imgq_r, imgq_c] = size(imgq);
+
+    % replace DC coefficients (stored in 1st col of each row in imgq) with
+    % the difference between itself and previous DC coefficient
+    DCdiff = zeros(1,imgq_r-1).';
+    for i = 2:imgq_r
+        DCdiff(i-1,1) = imgq(i,1) - imgq(i-1,1);
+    end
+    imgq(2:end,1) = DCdiff;
+
+    % store imgq in appropriate place in frameq
+    frameq(:,:,k) = imgq;
+
+end
+
+% encode the DCT coefficients on each frame
+entropy_enc(M, N, frameq, bitstream_name, N_images, quality);
 
 end
