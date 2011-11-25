5f9f7cbbe6e210c970ccc136c7d15263e065a26b
Graham Holland <graham.holland@gmail.com>
2011-11-25 03:43:56 -0800

    testing with order = [1 4 2 3 7 5 6] types = [I B B I B B I]

--- init_frame_info.m
+++ init_frame_info.m
@@ -3,17 +3,17 @@ function [frames, types, order] = init_frame_info()
 %   frames = INIT_FRAMES(order) Detailed explanation goes here.
 
 % encode/decode order by frame number
-order = [4 3 2 1 5 6 7];
+order = [1 4 2 3 7 5 6];
 N_images = length(order);
 
 % frame types
-types = ['P' 'P' 'P' 'I' 'P' 'P' 'P'];
+types = ['I' 'B' 'B' 'I' 'B' 'B' 'I'];
 types = types(order);
 
-fwd_ref = [2 3 4 0 4 5 6];
+fwd_ref = [0 1 1 0 4 4 0];
 fwd_ref = fwd_ref(order);
 
-back_ref = [0 0 0 0 0 0 0];
+back_ref = [0 4 4 0 7 7 0];
 back_ref = back_ref(order);
 
 % forward and backward weights
