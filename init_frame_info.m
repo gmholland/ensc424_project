aa7f5b47a8c213faea408e774cbc5a4a899bddfc
Graham Holland <graham.holland@gmail.com>
2011-11-25 21:05:12 -0800

    trying same config as (27) with P search range = 24

--- init_frame_info.m
+++ init_frame_info.m
@@ -3,23 +3,23 @@ function [frames, types, order] = init_frame_info()
 %   frames = INIT_FRAMES(order) Detailed explanation goes here.
 
 % encode/decode order by frame number
-order = [1 3 2 5 4 7 6];
+order = [4 3 2 1 5 6 7];
 N_images = length(order);
 
 % frame types
-types = ['I' 'B' 'P' 'B' 'P' 'B' 'P'];
+types = ['P' 'P' 'P' 'I' 'P' 'P' 'P'];
 types = types(order);
 
-fwd_ref = [0 1 1 3 3 5 5];
+fwd_ref = [2 3 4 0 4 5 6];
 fwd_ref = fwd_ref(order);
 
-back_ref = [0 3 0 5 0 7 0];
+back_ref = [0 0 0 0 0 0 0];
 back_ref = back_ref(order);
 
 % forward and backward weights
-wf = [0 1/2 0 1/2 0 1/2 0];
+wf = [0 0 0 0 0 0 0];
 wf = wf(order);
-wb = [0 1/2 0 1/2 0 1/2 0];
+wb = [0 0 0 0 0 0 0];
 wb = wb(order);
 
 %%%% encode/decode order by frame number
